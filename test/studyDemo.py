462c36518f3769be82939092299ffd1fef7645b4
tuomiyuqi <1447149059@qq.com>
2022-08-22 01:14:47 +0800

    练习

--- test/studyDemo.py
+++ test/studyDemo.py
@@ -1,6 +1,24 @@
-import keyword
-
-print(keyword.kwlist)
+import sys
 
 
-print([_ for _ in range(10)])
+class OpeatorSystem():
+    pass
+    # 新增
+
+    # 删除
+
+    # 更新
+
+    # 查询全部
+
+    # 指定查询
+
+
+if __name__ == '__main__':
+    # 实例化对象
+    opts = OpeatorSystem()
+
+    selectNum = int(input('1.新增 2.删除 3.更新 4.查询ALL 5.查询one 6.中止'))
+
+    if selectNum ==1:
+
